09a7d3009177092e5669590a45b2c34586d5dec7
genead <jirabona990@gmail.com>
2023-07-18 14:03:03 +0000

    completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -43,10 +43,13 @@ public class Lab {
         String buildingString = "";
 
 //        code here
+
     for(int i = start; i < end; i++){   
-        System.out.print(i);
+       // System.out.print(i);
+        buildingString = buildingString + i;
         
     }
+    
     return buildingString; 
 
     }
